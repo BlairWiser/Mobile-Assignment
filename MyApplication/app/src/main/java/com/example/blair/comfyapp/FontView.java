7a2881324225701949b5692aa376834b651e2c17
BlairWiser <blair.wiser@gmail.com>
2015-11-27 20:37:52 -0500

    Added title as a custom font

---
+++ MyApplication/app/src/main/java/com/example/blair/comfyapp/FontView.java
@@ -0,0 +1,37 @@
+package com.example.blair.comfyapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 100455689 on 11/27/2015.
+ */
+public class FontView extends View {
+
+    Typeface typeface = Typeface.createFromAsset(getContext().getAssets(),"Rakoon_PersonalUse.ttf");
+
+    public FontView(Context context) {
+        super(context);
+    }
+
+    public FontView(Context context, AttributeSet attribs) {
+        super(context);
+    }
+
+    public void onDraw(Canvas canvas) {
+        canvas.drawRGB(250, 250, 250);
+
+        Paint font = new Paint();
+        font.setTypeface(typeface);
+        font.setTextSize(150f);
+        font.setARGB(255, 25, 50, 100);
+        font.setStyle(Paint.Style.STROKE);
+
+        int x = 100, y = 100;
+        canvas.drawText("Comfy App", x, y, font);
+    }
+}
--- MyApplication/app/src/main/java/com/example/blair/comfyapp/MainMenu.java
+++ MyApplication/app/src/main/java/com/example/blair/comfyapp/MainMenu.java
@@ -1,9 +1,12 @@
 package com.example.blair.comfyapp;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,6 +14,8 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
     }
 
     @Override
@@ -34,4 +39,5 @@ public class MainMenu extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
