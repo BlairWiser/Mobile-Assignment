efeeb3236a842e9b8546b81382cec4b69b3bfa52
AdamAnthony <adam.a.pacheco@gmail.com>
2015-11-30 12:18:49 -0500

    Added a picker to choose from gallery
    Not sure if it fully works, need to push to test on a real device.

--- MyApplication/app/src/main/java/com/example/blair/comfyapp/Preferences.java
+++ MyApplication/app/src/main/java/com/example/blair/comfyapp/Preferences.java
@@ -2,8 +2,10 @@ package com.example.blair.comfyapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +13,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.net.URI;
+import java.net.URL;
 
 public class Preferences extends AppCompatActivity{
 
@@ -30,6 +35,27 @@ public class Preferences extends AppCompatActivity{
         editor.putString("urlName", urlName);
         boolean result = editor.commit();
         finish();
+    }
+
+    public void pickImage(View view){
+        Intent pickPicIntent = new Intent(Intent.ACTION_PICK);
+        pickPicIntent.setType("image/*");
+        startActivityForResult(pickPicIntent, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == 1){
+            if (resultCode == Activity.RESULT_OK){
+                Uri selectedPic = intent.getData();
+                String uriName = selectedPic.getPath();
+                SharedPreferences pref = getSharedPreferences("ComfyApp", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("urlName", uriName);
+                boolean result = editor.commit();
+                finish();
+            }
+        }
     }
 
     public void backToMenu(View view) {
